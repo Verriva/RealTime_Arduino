455dc65953927da68f715666b2431bbd3b2f089b
Verriva <echeverri.va@gmail.com>
2017-10-11 14:42:11 -0500

    Ultima Actualizacion

--- RealTime.cpp
+++ RealTime.cpp
@@ -1,5 +1,10 @@
 #include "RealTime.h"
 
+bool Flag = 0;
+bool Salida = 0;
+
+float Steps = 0;
+
 float Promedio_ADC(const char Canal, char Muestras)
 {
   /*  Esta funcion realiza un promedio de una lectura de cualquier puerto
@@ -16,6 +21,131 @@ float Promedio_ADC(const char Canal, char Muestras)
     Promedio += Mediciones[i];
   }
   return Promedio /= Muestras;
+}
+
+float Sensor_LM35(float Sensor)
+{
+  /*  Esta funcion realiza la conversion de la lectura del ADC a temperatura
+      del sensor LM35.                                                    */
+
+  Sensor = (5.0 * Sensor * 100.0)/1024;
+  return Sensor;
+
+}
+
+void Puente_H_L298(const char IN1, const char IN2, const char SENTIDO, bool Enable, const char DutyCicle)
+{
+  if(SENTIDO == 'D' && Enable == HIGH)
+  {
+    //analogWrite(IN1, DutyCicle);
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  }
+  else if(SENTIDO == 'I' && Enable == HIGH)
+  {
+    //analogWrite(IN2, DutyCicle);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }
+  else
+  {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+  }
+
+}
+
+bool Control_ON_OFF(float SetPoint, float Medicion)
+{
+  /*  Esta funcion realiza el control ON-OFF, evaluando el valor de entrada
+      con respecto al valor esperado, si el valor medido sobrepasa el valor
+      esperado esta funcion regresara un valor VERDADERO :O                */
+  bool Actuador = 0;
+
+  if(Medicion < SetPoint)
+  {
+    return Actuador = 0;
+  }
+  else if (Medicion > SetPoint)
+  {
+    return Actuador = 1;
+  }
+}
+
+bool Histeresis(float Entrada, float Limite, bool Estado)
+{
+  /*  Esta funcion regresa un valor booleano  Verdadero dependiendo si el
+      valor de  Entrada es Verdadero en cuyo caso activara una bandera que
+      mantendra el mismo valor booleano a la salida, hasta que la Entrada
+      sea menor que el Limite.
+
+      Esta funcion fue creada para complementarla con la funcion:
+      Control_ON_OFF(float SetPoint, float Medicion), pero tambien puede ser
+      utilizada en otra aplicacion.                   */
+
+      if((Entrada >= Limite) && (Estado == 1))
+      {
+        Flag = 1;
+        Salida = 1;
+      }
+
+      if(Flag == 1)
+      {
+        if(Entrada < Limite)
+        {
+          Flag = 0;
+          return Salida = 0;
+        }
+        return Salida = 1;
+      }
+}
+
+float Motor_Pasos(const char IN1, const char IN2, const char IN3, const char IN4, const char Direccion, int Delay)
+{
+
+  const char Puerto[4] = {IN1, IN2, IN3, IN4};
+
+  int Paso[4][4] =
+  {
+    {1,0,0,0},
+    {0,1,0,0},
+    {0,0,1,0},
+    {0,0,0,1}
+  };
+
+  if(Direccion == 'D')
+  {
+    for(int i = 0; i < 4; i ++)
+    {
+      for(int j = 0; j < 4; j ++)
+      {
+        digitalWrite(Puerto[j], Paso[i][j]);
+        //Serial.print(Paso[i][j]);
+      }
+      //Serial.println("    DERECHA");
+      //delay(Delay);
+      delayMicroseconds(Delay);
+      Steps--;
+    }
+  }
+  if(Direccion == 'I')
+  {
+    for(int i = 3; i >= 0; i --)
+    {
+      for(int j = 3; j >= 0; j --)
+      {
+        digitalWrite(Puerto[j], Paso[i][j]);
+        //Serial.print(Paso[i][j]);
+      }
+      //Serial.println("    IZQUIERDA");
+      //delay(Delay);
+      delayMicroseconds(Delay);
+      Steps++;
+    }
+  }
+
+  return Steps;
+
 }
 
 unsigned char Puerto(const char Puerto, char Pines, const char Estado)
@@ -38,7 +168,7 @@ unsigned char Puerto(const char Puerto, char Pines, const char Estado)
       pinMode(i, Estado);
       Lectura_Pines = digitalRead(i);
       Lectura_Pines = Lectura_Pines >> (j - 2);
-      Lectura_Port += Lectura_Pines;
+      Lectura_Port |= Lectura_Pines;
     }
   }
   else
@@ -51,7 +181,7 @@ unsigned char Puerto(const char Puerto, char Pines, const char Estado)
         pinMode(i, Estado);
         Lectura_Pines = digitalRead(i);
         Lectura_Pines = Lectura_Pines >> (j - 8);
-        Lectura_Port += Lectura_Pines;
+        Lectura_Port |= Lectura_Pines;
     }
   }
 
--- RealTime.h
+++ RealTime.h
@@ -6,6 +6,16 @@
 
 float Promedio_ADC(const char Canal, char Muestras);
 
+float Sensor_LM35(float Sensor);
+
+void Puente_H_L298(const char IN1, const char IN2, const char SENTIDO, bool Enable, const char DutyCicle);
+
+bool Control_ON_OFF(float SetPoint, float Medicion);
+
+bool Histeresis(float Entrada, float Limite, bool Estado);
+
+float Motor_Pasos(const char IN1, const char IN2, const char IN3, const char IN4, const char Direccion, int Delay);
+
 unsigned char Puerto(const char Puerto, char Pines, const char Estado);
 
 #endif
